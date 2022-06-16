d984e81557b60be1fecb91fd42090947401c5e1c
Bolawole <andubolawole@gmail.com>
2022-06-16 19:57:34 -0400

    Update the max-width of some sections of the pages.

---
+++ scripts/index-page.js
@@ -0,0 +1,31 @@
+const comments = [
+    {
+        name: 'Connor Walton',
+        timestamp: '02/17/2021',
+        comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.'
+    }
+    {
+        name: 'Emilie Beach',
+        timestamp: '01/09/2021',
+        comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.'
+    }
+];
+
+/* <div class="comment__old">
+        <img src="#" alt="profile picture" class="comment__avatar comment__avatar--new">
+        <div>
+            <div class="comment__info">
+                <div class="comment__writer">
+                    Connor Walton
+                </div>
+                <div class="comment__date">
+                    02/17/2021
+                </div>
+            </div>
+            <p>
+                This is art. 
+                This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. 
+                Let us appreciate this for what it is and what it contains.
+            </p>
+        </div>
+    </div> */
